9a22c9ca26c68fb026647a5e400b73f700e15e97
大冰块 <wujing0510@live.com>
2025-02-18 20:02:30 +0800

    source/comments/waline.js

--- source/comments/waline.js
+++ source/comments/waline.js
@@ -2,7 +2,7 @@ import { init, commentCount  } from 'https://unpkg.com/@waline/client@v3/dist/wa
 import { pageviewCount } from 'https://unpkg.com/@waline/client@v3/dist/pageview.js';
 
 (() => {
-  const serverURL = 'https://papwuj.github.io';
+  const serverURL = 'https://lzijc9kj.api.lncldglobal.com';
   let firstVisit = true;
 
   const loadComments = async () => {
